75a9dd4ea0d8b7f62e0ca0704941be6eea740380
aadilllkkkhhhaaannn <hamzakhannoor76@gmail.com>
2025-08-30 16:46:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+1. // Destructuring - array ya object me se value get karna
+
+// const user = {
+//   name: "Aadil",
+//   age: 22,
+// };
+// const { name, age } = user;
+// console.log(name, age);
+
+// const arr = [10, 20, 30];
+// const [first, second] = arr;
+
+// console.log(first, second);
+
+2. // spread operator (...) ka main kaam hota hai copy karna ya merge karna arrays/objects ko.
+// spread operator - kisi array ya object ko "spread" (phaila dena / copy ya merge karna).
+// const user = {
+//   name: "Aadil",
+//   age: 22,
+// };
+
+// const extra = {
+//   city: "indore",
+// };
+
+// const newUser = { ...user, ...extra };
+// console.log(newUser);
+
+3. // Template strings → ${ } ke sath string likhna
+// `Hello ${name}, age is ${age}`
