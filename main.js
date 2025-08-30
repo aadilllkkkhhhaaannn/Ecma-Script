e8e98692e30127b57107e44a5aeb037a03042f4e
aadilllkkkhhhaaannn <hamzakhannoor76@gmail.com>
2025-08-30 16:59:00 +0530

    rest operator

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-1. // Destructuring - array ya object me se value get karna
+1; // Destructuring - array ya object me se value get karna
 
 // const user = {
 //   name: "Aadil",
@@ -12,7 +12,7 @@
 
 // console.log(first, second);
 
-2. // spread operator (...) ka main kaam hota hai copy karna ya merge karna arrays/objects ko.
+2; // spread operator (...) ka main kaam hota hai copy karna ya merge karna arrays/objects ko.
 // spread operator - kisi array ya object ko "spread" (phaila dena / copy ya merge karna).
 // const user = {
 //   name: "Aadil",
@@ -26,5 +26,12 @@
 // const newUser = { ...user, ...extra };
 // console.log(newUser);
 
-3. // Template strings → ${ } ke sath string likhna
+3; // Template strings → ${ } ke sath string likhna
 // `Hello ${name}, age is ${age}`
+
+// 4. rest operator multiple values ko collect karke ek single array ya object bana dena (jaise bachi hui values ko "gather" karna).
+// const numbers = [1, 2, 3, 4, 5];
+// const [ first, second, ...rest] = numbers;
+// console.log(first);
+// console.log(second);
+// console.log(rest); // ye array me value lega
