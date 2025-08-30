7b78e1d77b78dba05822aab4dee903c0b0fa372d
aadilllkkkhhhaaannn <hamzakhannoor76@gmail.com>
2025-08-30 17:02:05 +0530

    code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-1; // Destructuring - array ya object me se value get karna
+1; // Destructuring
 
 // const user = {
 //   name: "Aadil",
@@ -12,8 +12,7 @@
 
 // console.log(first, second);
 
-2; // spread operator (...) ka main kaam hota hai copy karna ya merge karna arrays/objects ko.
-// spread operator - kisi array ya object ko "spread" (phaila dena / copy ya merge karna).
+2; // spread operator
 // const user = {
 //   name: "Aadil",
 //   age: 22,
@@ -26,10 +25,10 @@
 // const newUser = { ...user, ...extra };
 // console.log(newUser);
 
-3; // Template strings → ${ } ke sath string likhna
+3; // Template strings → ${ } 
 // `Hello ${name}, age is ${age}`
 
-// 4. rest operator multiple values ko collect karke ek single array ya object bana dena (jaise bachi hui values ko "gather" karna).
+// 4. rest oprator
 // const numbers = [1, 2, 3, 4, 5];
 // const [ first, second, ...rest] = numbers;
 // console.log(first);
